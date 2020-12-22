71a553b28f7f5b50bd9ff4d8f90b28af406ca316
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-22 23:28:24 +0800

    Update Decimal_to_Octal.cpp

--- Decimal_to_Octal.cpp
+++ Decimal_to_Octal.cpp
@@ -2,18 +2,6 @@
 
 using namespace std;
 
-int Power(int a, int b) {
-	int res = 1;
-	while (b > 0) {
-		if (b % 2 == 1) {
-			res *= a;
-		}
-		a *= a;
-		b /= 2;
-	}
-	return res;
-}
-
 void Solve(int n) {
 	if (n > 0) {
 		Solve(n / 8);
