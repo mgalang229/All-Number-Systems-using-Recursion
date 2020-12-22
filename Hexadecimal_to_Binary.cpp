eb60cf33baf9f16c6975c71d5caf937c12e602a5
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-22 23:32:34 +0800

    Update Hexadecimal_to_Binary.cpp

--- Hexadecimal_to_Binary.cpp
+++ Hexadecimal_to_Binary.cpp
@@ -1,45 +1,50 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
-using namespace __gnu_pbds;
 
-#define ll long long
-#define ar array
+int d = 0;
+int b = 1;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
+int Power(int a, int b) {
+	int res = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
+	}
+	return res;
+}
 
-int d=0, b=1;
-
-int solve(string s, int i) {
-	if(i>=0) {
-		if(s[i]>='0'&&s[i]<='9')
-			d+=(s[i]-48)*b;
-		else if(s[i]>='A'&&s[i]<='F')
-			d+=(s[i]-55)*b;
-		else if(s[i]>='a'&&s[i]<='f')
-			d+=(s[i]-87)*b;
-		b*=16;
-		solve(s, i-1);
+int Solve(string s, int i) {
+	if (i >= 0) {
+		if (s[i] >= '0' && s[i] <= '9') {
+			d += (s[i] - 48) * b;
+		} else if (s[i] >= 'A' && s[i] <= 'F') {
+			d += (s[i] - 55) * b;
+		} else if (s[i] >= 'a' && s[i] <= 'f') {
+			d += (s[i] - 87) * b;
+		}
+		b *= 16;
+		Solve(s, i - 1);
 	}
 	return d;
 }
 
-void solve2(int n) {
-	if(n) {
-		solve2(n/2);
-		cout << n%2;
+void Solve2(int n) {
+	if (n > 0) {
+			Solve2(n / 2);
+			cout << n % 2;
 	}
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	//hexadecimal->decimal->binary process
+	// hexadecimal to decimal to binary process
 	string s;
 	cin >> s;
-	int id=(int)s.size()-1;
-	solve2(solve(s, id));
-	cout << "\n";
+	int id = (int) s.size() - 1;
+	Solve2(Solve(s, id));
+	cout << '\n';
+	return 0;
 }
