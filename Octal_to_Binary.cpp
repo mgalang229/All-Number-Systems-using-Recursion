37f39e23bdde33e3c3a9d4ad13a4eeb003eb4ae1
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-22 23:37:56 +0800

    Update Octal_to_Binary.cpp

--- Octal_to_Binary.cpp
+++ Octal_to_Binary.cpp
@@ -1,50 +1,44 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
-using namespace __gnu_pbds;
 
-#define ll long long
-#define ar array
+int d = 0;
+int rem = 0;
+int i = 0;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
-
-int d=0, rem=0, i=0;
-
-int power(int a, int b) {
-	int res=1;
-	while(b>0) {
-		if(b&1)
-			res*=a;
-		a*=a;
-		b/=2;
+int Power(int a, int b) {
+	int res = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
 	}
 	return res;
 }
 
-int solve(int n) {
-	if(n) {
-		rem=n%10;
-		d+=rem*power(8, i++);
-		solve(n/10);
+int Solve(int n) {
+	if (n > 0) {
+		rem = n % 10;
+		d += rem * Power(8, i++);
+		Solve(n / 10);
 	}
 	return d;
 }
 
-void solve2(int n) {
-	if(n) {
-		solve2(n/2);
-		cout << n%2;
+void Solve2(int n) {
+	if (n > 0) {
+		Solve2(n / 2);
+		cout << n % 2;
 	}
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	//octal->decimal->binary process
+	// octal to decimal to binary process
 	int n;
 	cin >> n;
-	solve2(solve(n));
-	cout << "\n";
+	Solve2(Solve(n));
+	cout << '\n';
+	return 0;
 }
