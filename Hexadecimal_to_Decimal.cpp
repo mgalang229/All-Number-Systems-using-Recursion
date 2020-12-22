c46b074e479cb858302ba6b1094a7470fea637ce
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-22 23:33:58 +0800

    Update Hexadecimal_to_Decimal.cpp

--- Hexadecimal_to_Decimal.cpp
+++ Hexadecimal_to_Decimal.cpp
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
-using namespace __gnu_pbds;
 
-#define ll long long
-#define ar array
+int d = 0;
+int b = 1;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
-
-int d=0, b=1;
-
-int solve(string s, int i) {
-	if(i>=0) {
-		if(s[i]>='0'&&s[i]<='9')
-			d+=(s[i]-48)*b;
-		else if(s[i]>='A'&&s[i]<='F')
-			d+=(s[i]-55)*b;
-		else if(s[i]>='a'&&s[i]<='f')
-			d+=(s[i]-87)*b;
-		b*=16;
-		solve(s, i-1);
+int Solve(string s, int i) {
+	if (i >= 0) {
+		if (s[i] >= '0' && s[i] <= '9') {
+			d += (s[i] - 48) * b;
+		} else if (s[i] >= 'A' && s[i] <= 'F') {
+			d += (s[i] - 55) * b;
+		} else if (s[i] >= 'a' && s[i] <= 'f') {
+			d += (s[i] - 87) * b;
+		}
+		b *= 16;
+		Solve(s, i - 1);
 	}
 	return d;
 }
 
+void Solve2(int n) {
+	if (n > 0) {
+			Solve2(n / 2);
+			cout << n % 2;
+	}
+}
+
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
 	string s;
 	cin >> s;
-	int id=(int)s.size()-1;
-	cout << solve(s, id) << "\n";
+	int id = (int) s.size() - 1;
+	cout << Solve(s, id) << '\n';
+	return 0;
 }
