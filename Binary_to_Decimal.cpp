babb7cb5d5d6c3de583959c4ef6ce5a2c0d26c36
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-22 23:19:40 +0800

    Update Binary_to_Decimal.cpp

--- Binary_to_Decimal.cpp
+++ Binary_to_Decimal.cpp
@@ -1,42 +1,35 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
-using namespace __gnu_pbds;
 
-#define ll long long
-#define ar array
+long long d = 0;
+int rem = 0;
+int i = 0;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
-
-int d=0, rem=0, i=0;
-
-int power(int a, int b) {
-	int res=1;
-	while(b>0) {
-		if(b&1)
-			res*=a;
-		a*=a;
-		b/=2;
+int Power(int a, int b) {
+	int res = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
 	}
 	return res;
 }
 
-int solve(int n) {
-	if(n) {
-		rem=n%10;
-		d+=rem*power(2, i++);
-		solve(n/10);
+long long Solve(long long n) {
+	if (n > 0) {
+		rem = n % 10;
+		d += rem * Power(2, i++);
+		Solve(n / 10);
 	}
 	return d;
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	
-	ll n;
+	long long n;
 	cin >> n;
-	cout << solve(n) << "\n";
+	cout << Solve(n) << '\n';
+	return 0;
 }
