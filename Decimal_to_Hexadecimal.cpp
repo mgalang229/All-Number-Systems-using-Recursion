1426b2989fcb192fc10ada98faadc6a97d25586a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-24 00:23:07 +0800

    Update Decimal_to_Hexadecimal.cpp

--- Decimal_to_Hexadecimal.cpp
+++ Decimal_to_Hexadecimal.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-void Solve(int n) {
-	if (n > 0) {
-		int tmp = n % 16;
-		Solve(n / 16);
-		if (tmp< 10) {
-			cout << char(48 + tmp);
-		} else {
-			cout << char(55 + tmp);
-		}
+#define ll long long
+#define ar array
+
+void solve(int n) {
+	if(n) {
+		int tmp=n%16;
+		solve(n/16);
+		if(tmp<10)
+			cout << char(48+tmp);
+		else
+			cout << char(55+tmp);
 	}
 }
 
 int main() {
 	int n;
 	cin >> n;
-	Solve(n);
-	cout << '\n';
-	return 0;
+	solve(n);
+	cout << "\n";
 }
