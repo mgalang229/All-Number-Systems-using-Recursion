7a714942fc2124970018d769301dbc9068fbf90b
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-24 00:30:55 +0800

    Update Octal_to_Hexadecimal.cpp

--- Octal_to_Hexadecimal.cpp
+++ Octal_to_Hexadecimal.cpp
@@ -1,49 +1,46 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int d = 0;
-int rem = 0;
-int i = 0;
+#define ll long long
+#define ar array
 
-int Power(int a, int b) {
-	int res = 1;
-	while (b > 0) {
-		if (b % 2 == 1) {
-			res *= a;
-		}
-		a *= a;
-		b /= 2;
+int d=0, rem=0, i=0;
+
+int power(int a, int b) {
+	int res=1;
+	while(b) {
+		if(b&1)
+			res*=a;
+		a*=a;
+		b/=2;
 	}
 	return res;
 }
 
-int Solve(int n) {
-	if (n > 0) {
-		rem = n % 10;
-		d += rem * Power(8, i++);
-		Solve(n / 10);
+int solve(int n) {
+	if(n) {
+		rem=n%10;
+		d+=rem*power(8, i++);
+		solve(n/10);
 	}
 	return d;
 }
 
-void Solve2(int n) {
-	if (n > 0) {
-		int tmp = n % 16;
-		Solve2(n / 16);
-		if (tmp < 10) {
-			cout << char(48 + tmp);
-		} else {
-			cout << char(55 + tmp);
-		}
+void solve2(int n) {
+	if(n) {
+		int tmp=n%16;
+		solve2(n/16);
+		if(tmp<10)
+			cout << char(48+tmp);
+		else
+			cout << char(55+tmp);
 	}
 }
 
 int main() {
-	// octal to decimal to hexadecimal process
+	//octal->decimal->hex process
 	int n;
 	cin >> n;
-	Solve2(Solve(n));
-	cout << '\n';
-	return 0;
+	solve2(solve(n));
+	cout << "\n";
 }
