4bf4700782b09857617383733be82898ce301d62
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-24 00:28:05 +0800

    Update Hexadecimal_to_Decimal.cpp

--- Hexadecimal_to_Decimal.cpp
+++ Hexadecimal_to_Decimal.cpp
@@ -1,36 +1,41 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int d = 0;
-int b = 1;
+#define ll long long
+#define ar array
 
-int Solve(string s, int i) {
-	if (i >= 0) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			d += (s[i] - 48) * b;
-		} else if (s[i] >= 'A' && s[i] <= 'F') {
-			d += (s[i] - 55) * b;
-		} else if (s[i] >= 'a' && s[i] <= 'f') {
-			d += (s[i] - 87) * b;
-		}
-		b *= 16;
-		Solve(s, i - 1);
+//int d=0, rem=0, i=0;
+
+int power(int a, int b) {
+	int res=1;
+	while(b) {
+		if(b&1)
+			res*=a;
+		a*=a;
+		b/=2;
 	}
-	return d;
+	return res;
 }
 
-void Solve2(int n) {
-	if (n > 0) {
-			Solve2(n / 2);
-			cout << n % 2;
+int d=0, b=1;
+
+int solve(string s, int i) {
+	if(i>=0) {
+		if(s[i]>='0'&&s[i]<='9')
+			d+=(s[i]-48)*b;
+		else if(s[i]>='A'&&s[i]<='F')
+			d+=(s[i]-55)*b;
+		else if(s[i]>='a'&&s[i]<='f')
+			d+=(s[i]-87)*b;
+		b*=16;
+		solve(s, i-1);
 	}
+	return d;
 }
 
 int main() {
 	string s;
 	cin >> s;
-	int id = (int) s.size() - 1;
-	cout << Solve(s, id) << '\n';
-	return 0;
+	int id=(int)s.size()-1;
+	cout << solve(s, id) << "\n";
 }
