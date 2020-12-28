3a5b5067f826aff539151fe2dc0a65a89697dc66
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-28 17:47:12 +0800

    Update Binary_to_Decimal.cpp

--- Binary_to_Decimal.cpp
+++ Binary_to_Decimal.cpp
@@ -1,33 +1,35 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define ar array
+int d = 0;
+int rem = 0;
+int i = 0;
 
-int d=0, rem=0, i=0;
-
-int power(int a, int b) {
-	int res=1;
-	while(b) {
-		if(b&1)
-			res*=a;
-		a*=a;
-		b/=2;
+int Power(int a, int b) {
+	long long res = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
 	}
 	return res;
 }
 
-int solve(ll n) {
-	if(n) {
-		rem=n%10;
-		d+=rem*power(2, i++);
-		solve(n/10);
+int Convert(long long n) {
+	if (n > 0) {
+		rem = n % 10;
+		d += rem * Power(2, i++);
+		Convert(n / 10);
 	}
 	return d;
 }
 
 int main() {
-	ll n;
+	long long n;
 	cin >> n;
-	cout << solve(n) << "\n";
+	cout << Convert(n) << '\n';
+	return 0;	
 }
