4d84325614fb2ca165e8bf4fc6d723b064e23c12
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-28 17:49:48 +0800

    Update Binary_to_Hexadecimal.cpp

--- Binary_to_Hexadecimal.cpp
+++ Binary_to_Hexadecimal.cpp
@@ -1,46 +1,49 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define ar array
+int d = 0;
+int rem = 0;
+int i = 0;
 
-int d=0, rem=0, i=0;
-
-int power(int a, int b) {
-	int res=1;
-	while(b) {
-		if(b&1)
-			res*=a;
-		a*=a;
-		b/=2;
+int Power(int a, int b) {
+	long long res = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
 	}
 	return res;
 }
 
-int solve(ll n) {
-	if(n) {
-		rem=n%10;
-		d+=rem*power(2, i++);
-		solve(n/10);
+int Convert(long long n) {
+	if (n > 0) {
+		rem = n % 10;
+		d += rem * Power(2, i++);
+		Convert(n / 10);
 	}
 	return d;
 }
 
-void solve2(int n) {
-	if(n) {
-		int tmp=n%16;
-		solve2(n/16);
-		if(tmp<10)
-			cout << char(48+tmp);
-		else
-			cout << char(55+tmp);
+void Convert2(int n) {
+	if (n > 0) {
+		int tmp = n % 16;
+		Convert2(n / 16);
+		if (tmp < 10) {
+			cout << char(48 + tmp);
+		} else {
+			cout << char(55 + tmp);
+		}
 	}
 }
 
 int main() {
-	//binary->decimal->hex process
-	ll n;
+	// binary -> decimal -> hexadecimal process
+	long long n;
 	cin >> n;
-	solve2(solve(n));
-	cout << "\n";
+	Convert2(Convert(n));
+	cout << '\n';
+	return 0;	
 }
