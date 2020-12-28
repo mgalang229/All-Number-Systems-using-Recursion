f3d9731e60ba3dc8a15ae7b7c9413b68f4b9f8b5
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-28 17:50:49 +0800

    Update Binary_to_Octal.cpp

--- Binary_to_Octal.cpp
+++ Binary_to_Octal.cpp
@@ -1,42 +1,44 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define ar array
+int d = 0;
+int rem = 0;
+int i = 0;
 
-int d=0, rem=0, i=0;
-
-int power(int a, int b) {
-	int res=1;
-	while(b) {
-		if(b&1)
-			res*=a;
-		a*=a;
-		b/=2;
+int Power(int a, int b) {
+	long long res = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
 	}
 	return res;
 }
 
-int solve(ll n) {
-	if(n) {
-		rem=n%10;
-		d+=rem*power(2, i++);
-		solve(n/10);
+int Convert(long long n) {
+	if (n > 0) {
+		rem = n % 10;
+		d += rem * Power(2, i++);
+		Convert(n / 10);
 	}
 	return d;
 }
 
-void solve2(int n) {
-	if(n) {
-		solve2(n/8);
-		cout << n%8;
+void Convert2(int n) {
+	if (n > 0) {
+		Convert2(n / 8);
+		cout << n % 8;
 	}
 }
 
 int main() {
-	//binary->decimal->octal process
-	ll n;
+	// binary -> decimal -> octal process
+	long long n;
 	cin >> n;
-	solve2(solve(n));
-	cout << "\n";
+	Convert2(Convert(n));
+	cout << '\n';
+	return 0;	
 }
