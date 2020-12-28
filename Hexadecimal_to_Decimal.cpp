9b78769e6bb648332e9e15b57d06628fac2ab04d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-28 17:58:57 +0800

    Update Hexadecimal_to_Decimal.cpp

--- Hexadecimal_to_Decimal.cpp
+++ Hexadecimal_to_Decimal.cpp
@@ -1,34 +1,21 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define ar array
+int d = 0;
+int b = 1;
 
-//int d=0, rem=0, i=0;
-
-int power(int a, int b) {
-	int res=1;
-	while(b) {
-		if(b&1)
-			res*=a;
-		a*=a;
-		b/=2;
-	}
-	return res;
-}
-
-int d=0, b=1;
-
-int solve(string s, int i) {
-	if(i>=0) {
-		if(s[i]>='0'&&s[i]<='9')
-			d+=(s[i]-48)*b;
-		else if(s[i]>='A'&&s[i]<='F')
-			d+=(s[i]-55)*b;
-		else if(s[i]>='a'&&s[i]<='f')
-			d+=(s[i]-87)*b;
-		b*=16;
-		solve(s, i-1);
+int Convert(string s, int i) {
+	if (i >= 0) {
+		if (s[i] >= '0' && s[i] <= '9') {
+			d += (s[i] - 48) * b;
+		} else if (s[i] >= 'A' && s[i] <= 'F') {
+			d += (s[i] - 55) * b;
+		} else if (s[i] >= 'a' && s[i] <= 'f') {
+			d += (s[i] - 87) * b;
+		}
+		b *= 16;
+		Convert(s, i - 1);
 	}
 	return d;
 }
@@ -36,6 +23,7 @@ int solve(string s, int i) {
 int main() {
 	string s;
 	cin >> s;
-	int id=(int)s.size()-1;
-	cout << solve(s, id) << "\n";
+	int id = s.size() - 1;
+	cout << Convert(s, id) << '\n';
+	return 0;	
 }
