319de4f6486204d33aee0846824f01c5864cb89a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-28 17:53:18 +0800

    Update Decimal_to_Octal.cpp

--- Decimal_to_Octal.cpp
+++ Decimal_to_Octal.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define ar array
-
-void solve(int n) {
-	if(n) {
-		solve(n/8);
-		cout << n%8;
+void Convert(int n) {
+	if (n > 0) {
+		Convert(n / 8);
+		cout << n % 8;
 	}
 }
 
 int main() {
 	int n;
 	cin >> n;
-	solve(n);
-	cout << "\n";
+	Convert(n);
+	cout << '\n';
+	return 0;	
 }
