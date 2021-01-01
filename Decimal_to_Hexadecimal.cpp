691052d26cf3e33cc3e27b6a9d2f85adc8e89179
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-01 20:44:48 +0800

    Update Decimal_to_Hexadecimal.cpp

--- Decimal_to_Hexadecimal.cpp
+++ Decimal_to_Hexadecimal.cpp
@@ -1,23 +1,26 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-void Convert(int n) {
-	if (n > 0) {
-		int tmp = n % 16;
-		Convert(n / 16);
-		if (tmp < 10) {
-			cout << char(48 + tmp);
-		} else {
-			cout << char(55 + tmp);
-		}
+#define ll long long
+#define ar array
+
+void solve(int n) {
+	if(n) {
+		int tmp=n%16;
+		solve(n/16);
+		if(tmp<10)
+			cout << char(48+tmp);
+		else
+			cout << char(55+tmp);
 	}
 }
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
 	int n;
 	cin >> n;
-	Convert(n);
-	cout << '\n';
-	return 0;	
+	solve(n);
+	cout << "\n";
 }
