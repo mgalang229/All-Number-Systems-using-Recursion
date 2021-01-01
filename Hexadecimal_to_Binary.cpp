3827621f786038e89be6417b1b50c815c5c9e7fc
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-01 20:49:06 +0800

    Update Hexadecimal_to_Binary.cpp

--- Hexadecimal_to_Binary.cpp
+++ Hexadecimal_to_Binary.cpp
@@ -1,38 +1,40 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int d = 0;
-int b = 1;
+#define ll long long
+#define ar array
 
-int Convert(string s, int i) {
-	if (i >= 0) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			d += (s[i] - 48) * b;
-		} else if (s[i] >= 'A' && s[i] <= 'F') {
-			d += (s[i] - 55) * b;
-		} else if (s[i] >= 'a' && s[i] <= 'f') {
-			d += (s[i] - 87) * b;
-		}
-		b *= 16;
-		Convert(s, i - 1);
+int d=0, b=1;
+
+int solve(string s, int i) {
+	if(i>=0) {
+		if(s[i]>='0'&&s[i]<='9')
+			d+=(s[i]-48)*b;
+		else if(s[i]>='A'&&s[i]<='F')
+			d+=(s[i]-55)*b;
+		else if(s[i]>='a'&&s[i]<='f')
+			d+=(s[i]-87)*b;
+		b*=16;
+		solve(s, i-1);
 	}
 	return d;
 }
 
-void Convert2(int n) {
-	if (n > 0) {
-		Convert2(n / 2);
-		cout << n % 2;
+void solve2(int n) {
+	if(n) {
+		solve2(n/2);
+		cout << n%2;
 	}
 }
 
 int main() {
-	// hexdecimal -> decimal -> binary process
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	//hex->decimal->binary process
 	string s;
 	cin >> s;
-	int id = s.size() - 1;
-	Convert2(Convert(s, id));
-	cout << '\n';
-	return 0;	
+	int id=(int)s.size()-1;
+	solve2(solve(s, id));
+	cout << "\n";
 }
