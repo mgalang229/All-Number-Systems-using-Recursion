ebb5c0ae828906fccf621769b578d6810ae2238b
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-07 23:38:23 +0800

    Update Octal_to_Decimal.cpp

--- Octal_to_Decimal.cpp
+++ Octal_to_Decimal.cpp
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define ar array
-
-int power(int a, int b) {
-	int res=1;
-	while(b) {
-		if(b&1)
-			res*=a;
-		a*=a;
-		b/=2;
+int Power(int a, int b) {
+	int res = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
 	}
 	return res;
 }
 
-int d=0, rem=0, i=0;
+int d = 0;
+int rem = 0;
+int i = 0;
 
-int solve(int n) {
-	if(n) {
-		rem=n%10;
-		d+=rem*power(8, i++);
-		solve(n/10);
+int Convert(int n) {
+	if (n > 0) {
+		rem = n % 10;
+		d += rem * Power(8, i++);
+		Convert(n / 10);
 	}
 	return d;
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
 	int n;
 	cin >> n;
-	cout << solve(n) << "\n";
+	cout << Convert(n) << '\n';
+	return 0;
 }
